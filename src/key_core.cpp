0d0fffc8127e9f7daee97e0986b9292e20a512d2
Anthony Leedom <anthonym.leedom@gmail.com>
2023-10-19 22:55:46 -0700

    Revert MacOS key specialization

--- src/key_core.cpp
+++ src/key_core.cpp
@@ -140,7 +140,6 @@ auto const key_map = [] {
 
 #elif defined(__APPLE__) || defined(__MACH__)
     // macOS
-    x["delete"] = juce::KeyPress::backspaceKey;
 
 #elif defined(__linux__)
     // Linux
